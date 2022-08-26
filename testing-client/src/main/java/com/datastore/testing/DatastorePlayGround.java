e71a6da925a11e63e2a9c57cf71b1e0579adb5fd
Prateek Jain <jainsahab@google.com>
2022-08-26 15:46:00 +0530

    Playground to access DS count aggregation endpoints

---
+++ testing-client/src/main/java/com/datastore/testing/DatastorePlayGround.java
@@ -0,0 +1,83 @@
+package com.datastore.testing;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.EntityQuery;
+import com.google.cloud.datastore.QueryResults;
+import com.google.cloud.datastore.spi.v1.DatastoreRpc;
+import com.google.cloud.datastore.spi.v1.HttpDatastoreRpc;
+import com.google.datastore.v1.AggregationQuery;
+import com.google.datastore.v1.AggregationQuery.Aggregation;
+import com.google.datastore.v1.AggregationQuery.Aggregation.Count;
+import com.google.datastore.v1.EntityResult;
+import com.google.datastore.v1.Filter;
+import com.google.datastore.v1.KindExpression;
+import com.google.datastore.v1.PartitionId;
+import com.google.datastore.v1.PropertyFilter;
+import com.google.datastore.v1.PropertyFilter.Operator;
+import com.google.datastore.v1.PropertyReference;
+// import com.google.datastore.v1.Query;
+import com.google.datastore.v1.RunAggregationQueryRequest;
+import com.google.datastore.v1.RunAggregationQueryResponse;
+import com.google.datastore.v1.RunQueryRequest;
+import com.google.datastore.v1.RunQueryResponse;
+import com.google.datastore.v1.Value;
+import com.google.protobuf.Int64Value;
+import java.util.List;
+
+public class DatastorePlayGround {
+    public static void main(String[] args) {
+        DatastoreOptions datastoreOptions = DatastoreOptions.getDefaultInstance();
+        // Datastore datastore = datastoreOptions.getService();
+        // EntityQuery selectAllQuery = Query.newEntityQueryBuilder()
+        //     .setNamespace("1660851506815")
+        //     .setKind("Character")
+        //     .build();
+        // QueryResults<Entity> results = datastore.run(selectAllQuery);
+        // while (results.hasNext()) {
+        //     Entity entity = results.next();
+        //     System.out.println("Value is: " + entity.getKey());
+        // }
+
+
+        //Regular Select Query
+        DatastoreRpc datastoreRpc = new HttpDatastoreRpc(datastoreOptions);
+        PartitionId partitionId = PartitionId.newBuilder().setNamespaceId("1660851506815")
+            .setProjectId(datastoreOptions.getProjectId()).build();
+
+        com.google.datastore.v1.Query selectAllCharacterQuery = com.google.datastore.v1.Query.newBuilder()
+            .addKind(KindExpression.newBuilder().setName("Character").build())
+            .setFilter(Filter.newBuilder().setPropertyFilter(PropertyFilter.newBuilder().setProperty(PropertyReference.newBuilder().setName("name").build()).setOp(Operator.NOT_EQUAL).setValue(Value.newBuilder().setStringValue("Catelyn").build()).build()).build())
+            .build();
+        RunQueryRequest runqueryRequest = RunQueryRequest.newBuilder()
+            .setPartitionId(partitionId)
+            .setQuery(selectAllCharacterQuery)
+            .build();
+        RunQueryResponse runQueryResponse = datastoreRpc.runQuery(runqueryRequest);
+        List<EntityResult> entityResultsList = runQueryResponse.getBatch().getEntityResultsList();
+        for (EntityResult entityResult : entityResultsList) {
+            System.out.println("Value is: " + entityResult.getEntity().getPropertiesMap().get("name"));
+        }
+
+        //Count aggregation over select Query (with and without upTo)
+        Count count = Count.newBuilder()
+            .setUpTo(Int64Value.of(6))
+            .build();
+        Aggregation countAggregation = Aggregation.newBuilder().setCount(count).setAlias("total_count").build();
+        AggregationQuery aggregationQuery = AggregationQuery.newBuilder()
+            .addAggregations(countAggregation)
+            .setNestedQuery(selectAllCharacterQuery)
+            .build();
+
+        RunAggregationQueryRequest runAggregationQueryRequest = RunAggregationQueryRequest.newBuilder()
+            .setAggregationQuery(aggregationQuery)
+            .setPartitionId(partitionId)
+            .build();
+
+        RunAggregationQueryResponse runAggregationQueryResponse = datastoreRpc.runAggregationQuery(runAggregationQueryRequest);
+
+        System.out.println(runAggregationQueryResponse.getBatch().getAggregationResults(0).getAggregatePropertiesMap());
+
+    }
+}
